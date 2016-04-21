ac8feb512e38bbe1c596d84086a71ef3c13d3ffa
Zuiiey <lindasuihko@gmail.com>
2016-04-21 21:53:05 +0200

    First commit added folder

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		
+		<!-- jQuery CSS -->
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
+		
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+		
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+		
+		<!-- jQuery UI -->
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
+	</head>
+	<body>
+
+		
+		<nav class="navbar navbar-default" role="navigation">
+			<div>
+				<ul class="nav nav-pills">
+					<li role="presentation" class="dropdown">
+				    	<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+				     		<i class="fa fa-bars fa-3x" aria-hidden="true"></i><span></span>
+				    	</a>
+				    	<ul class="dropdown-menu">
+				      		<li href="#">Tasks</li>
+				      		<li href="#">My Profile</li>
+				      		<li href="#">Statistics</li>
+				      		<li role="separator" class="divider"></li>
+				      		<li href="#">Log out</li>
+				    	</ul>
+				  	</li>
+				  	<h1>AppName</h1>
+				</ul>					
+			</div>
+		</nav>
+		
+		<footer>
+			<div class="container">
+				<p>Copyright © Me 2016</p>
+			</div>
+		</footer>
+	</body>
+	
+	
+	
+</html>
